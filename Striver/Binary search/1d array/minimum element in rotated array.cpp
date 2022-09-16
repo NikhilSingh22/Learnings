4d7ebd87190451cb23a708183bd51da12f2a56da
Nikhil Singh <77776708+NikhilSingh22@users.noreply.github.com>
2022-09-16 22:50:53 +0530

    Update minimum element in rotated array.cpp

--- Striver/Binary search/1d array/minimum element in rotated array.cpp
+++ Striver/Binary search/1d array/minimum element in rotated array.cpp
@@ -1,25 +1,12 @@
-int binary_search(int arr[],int n)
-    {
-        int l =0;
-        int h= n-1;
+ int findMin(vector<int>& nums) {
+        int l = 0;
+        int h=nums.size()-1;
         while(l<h)
         {
             int m = l+(h-l)/2;
-          
-            if(arr[m]>arr[h])
-            {
+            if(nums[m]>nums[h])
                 l=m+1;
-                
-            }
-            else 
-            {
-                h=m;
-            }
+            else h=m;
         }
-        return arr[l];
-    }
-    
-    int findMin(int arr[], int n){
-       int mini = binary_search(arr,n);
-       return mini;
+        return nums[l];
     }
