bae85d1e5b31088ca474ca00d10745a6930adb59
Nikhil Singh <77776708+NikhilSingh22@users.noreply.github.com>
2022-09-29 00:23:46 +0530

    Create isomorphic strings.cpp

---
+++ Striver/Strings/Easy_basics/isomorphic strings.cpp
@@ -0,0 +1,24 @@
+// basically it is one to one mapping 
+
+bool areIsomorphic(string str1, string str2)
+    {
+        
+       if(str1.size()!=str2.size())
+        return 0;
+       map<char,int>mp1;
+       map<char,int>mp2;
+       
+       for(int i = 0;i<str1.size();i++)
+        {
+            mp1[str1[i]] = i+1; 
+            mp2[str2[i]]= i+1;
+        }
+        
+        
+        for(int i=0;i<str1.size();i++)
+        {
+            if( mp1[str1[i]] != mp2[str2[i]]) // if any key in the map is repeated so there index will no be equal and not isomorphic;
+            return 0;
+        }
+    return 1;
+    }
