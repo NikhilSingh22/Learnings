3f016191c429652542503461032d4ad835c79ccc
Nikhil Singh <77776708+NikhilSingh22@users.noreply.github.com>
2022-09-28 22:56:26 +0530

    Update Reverse word in a string.cpp

--- Striver/Strings/Easy_basics/Reverse word in a string.cpp
+++ Striver/Strings/Easy_basics/Reverse word in a string.cpp
@@ -19,3 +19,26 @@ string reverseWords(string s)
 
 
 // let code question in which word have extra spaces which should be removed while reversing the string of words;
+    string reverseWords(string s) {
+        
+        reverse(s.begin(),s.end());
+       int l = 0 , i = 0 ,r = 0, n = s.size();
+        
+        while(i<n)
+        {
+            while(i<n && s[i]!=' ')
+                s[r++]=s[i++];  // just performing shifting if there is gap other wise simple copy
+            
+            if(l<r)
+            {
+                reverse(s.begin()+l, s.begin()+r);  // reversing each word
+                if(r==n)break;
+                s[r++]=' ';
+                l=r;
+            }
+            i++; // ignoring space 
+        }
+        while(r>0 &&s[r-1]==' ')r--;
+        s.resize(r); // reducing the size of string after removing extra spaces;
+        return s;
+    }
