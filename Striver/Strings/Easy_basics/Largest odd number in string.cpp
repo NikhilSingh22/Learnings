5d4deab9f2f37ac4783286c95ea8b6fc0eebab7e
Nikhil Singh <77776708+NikhilSingh22@users.noreply.github.com>
2022-09-29 13:20:26 +0530

    Update Largest odd number in string.cpp

--- Striver/Strings/Easy_basics/Largest odd number in string.cpp
+++ Striver/Strings/Easy_basics/Largest odd number in string.cpp
@@ -4,8 +4,8 @@ string largestOddNumber(string num) {
         
         for(int i=n-1 ; i>=0 ;i--)
         {
-             int n = (int)num[i]; // converting a character in int
-                 if(n&1==1)
+             int x = (int)num[i]; // converting a character in int
+                 if(x&1==1)
                  {
                      str = num.substr(0,i+1);
                      break;
