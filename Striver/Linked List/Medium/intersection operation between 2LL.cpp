5e2997f9eb2ae6a140bf0f18b46246b8ed30a6ce
Nikhil Singh <77776708+NikhilSingh22@users.noreply.github.com>
2022-10-01 00:41:10 +0530

    Create intersection operation between 2LL.cpp

---
+++ Striver/Linked List/Medium/intersection operation between 2LL.cpp
@@ -0,0 +1,30 @@
+Node* findIntersection(Node* head1, Node* head2)
+    {
+        Node*temp1=head1;
+        Node*temp2=head2;
+        Node*dummy = new Node(-1);
+        Node *d =dummy;
+        
+        set<int>st;
+        while(temp2!=NULL)
+        {
+            st.insert(temp2->data);
+            temp2=temp2->next;
+        }
+        
+        
+        while(temp1!=NULL)
+        {
+            auto pos = st.find(temp1->data);
+            if(pos!=st.end())
+            {
+                Node *n = new Node(temp1->data);
+                d->next=n;
+                d=d->next;
+            }
+            
+            temp1=temp1->next;
+        }
+        
+        return dummy->next;
+    }
