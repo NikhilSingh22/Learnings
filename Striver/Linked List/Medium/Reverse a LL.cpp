31d16c9754fca6acd92e4300f3a9000b31c11265
Nikhil Singh <77776708+NikhilSingh22@users.noreply.github.com>
2022-09-30 17:53:23 +0530

    Update Reverse a LL.cpp

--- Striver/Linked List/Medium/Reverse a LL.cpp
+++ Striver/Linked List/Medium/Reverse a LL.cpp
@@ -17,3 +17,18 @@ struct Node* reverseList(struct Node *head)
         
         return prevptr;
     }
+
+
+
+
+// recursive reverse
+ ListNode* reverseList(ListNode* head) {
+        if(head==NULL||head->next==NULL)
+          return head;
+
+        ListNode* newhead=reverseList(head->next);
+        head->next->next=head;
+        head->next=NULL;
+
+        return newhead;
+ }
