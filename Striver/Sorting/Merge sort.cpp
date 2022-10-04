1af807cfa4673e7293f002318fc4142e78b61030
Nikhil Singh <77776708+NikhilSingh22@users.noreply.github.com>
2022-10-04 11:54:07 +0530

    Create Merge sort.cpp

---
+++ Striver/Sorting/Merge sort.cpp
@@ -0,0 +1,44 @@
+void merge(int arr[], int l, int m, int r)
+    {
+         int n1 = m-l+1;
+         int n2 = r-m;
+         
+         int arr1[n1],arr2[n2];
+         
+         for(int i = 0 ;i<n1;i++)
+         {
+             arr1[i]=arr[l+i];
+         }
+         for(int i = 0 ;i<n2;i++)
+         {
+             arr2[i]=arr[m+1+i];
+         }
+         int i = 0,j=0,k=l;
+         while(i<n1 && j<n2)
+         {
+             if(arr1[i]<=arr2[j])
+             arr[k++]=arr1[i++];
+             else
+             arr[k++]=arr2[j++];
+         }
+         
+         while(i<n1)
+         {
+             arr[k++]=arr1[i++];
+         }
+         while(j<n2)
+         {
+             arr[k++]=arr2[j++];
+         }
+    }
+    public:
+    void mergeSort(int arr[], int l, int r)
+    {
+        if(l<r)
+        {
+            int m = l+(r-l)/2;
+            mergeSort(arr,l,m);
+            mergeSort(arr,m+1,r);
+            merge(arr,l,m,r);
+        }
+    }
