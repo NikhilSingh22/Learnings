f97d359750c7483bd4389edb1e0b8d2de3c0e116
Nikhil Singh <77776708+NikhilSingh22@users.noreply.github.com>
2022-10-04 11:53:29 +0530

    Create Insertion sort.cpp

---
+++ Striver/Sorting/Insertion sort.cpp
@@ -0,0 +1,15 @@
+void insertionSort(int arr[], int n)
+    {
+        int i , temp , j;
+        for(i=1;i<n;i++)
+        {
+            temp =arr[i];
+            j=i-1;
+            while(j>=0 && arr[j]>temp)
+            {
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=temp;
+        }
+    }
