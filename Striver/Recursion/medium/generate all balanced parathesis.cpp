8279a8ee1021805d11bb650a775b41426b19fad8
Nikhil Singh <77776708+NikhilSingh22@users.noreply.github.com>
2022-10-11 22:46:52 +0530

    Create generate all balanced parathesis.cpp

---
+++ Striver/Recursion/medium/generate all balanced parathesis.cpp
@@ -0,0 +1,25 @@
+void solver(vector<string>&ans,string sol,int open,int close)
+    {
+        if(open == 0 && close ==0)
+        {
+            ans.push_back(sol);
+            return;
+        }
+        
+        if(open!=0)
+        {
+            solver(ans,sol+'(',open-1,close);
+        }
+        if(close>open)
+        {
+            solver(ans,sol+')',open,close-1);
+        }
+        
+    }
+    vector<string> AllParenthesis(int n) 
+    {
+       vector<string> ans;
+       string sol ="";
+       solver(ans, sol,n,n);
+       return ans;
+    }
